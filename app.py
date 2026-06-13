926a9d7b38071b71af8b96b011d79396f70f6148
praneeth1187-zoro <praneeth11877@gmail.com>
2026-06-13 16:13:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
-st.title("AI PDF Assistant")
+st.title("📄 AI PDF Assistant")
 
 uploaded_file = st.file_uploader(
     "Upload a PDF",
@@ -8,4 +9,19 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file:
-    st.success("PDF uploaded successfully!")
+    pdf = PdfReader(uploaded_file)
+
+    st.success(f"Uploaded: {uploaded_file.name}")
+    st.write(f"Number of Pages: {len(pdf.pages)}")
+
+    text = ""
+
+    for page in pdf.pages:
+        text += page.extract_text() or ""
+
+    st.subheader("Extracted Text")
+    st.text_area(
+        "PDF Content",
+        text,
+        height=300
+    )
